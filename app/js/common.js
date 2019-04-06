b8004a7f47616330a2887436cf56d41e46e66b2d
mediocre <druksasha@ukr.net>
2019-04-07 02:06:14 +0300

    )

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,6 @@
-
-$(window).on('load', function(){$("#my-menu").css('opacity','1');});
+$(window).on('load', function() {
+  $("#my-menu").css('opacity', '1');
+});
 $(function() {
   $(document).ready(function() {
     $("#my-menu").mmenu({
@@ -12,5 +13,18 @@ $(function() {
       },
       "slidingSubmenus": false
     });
+    $(".accordion_head").click(function(){
+		if ($('.accordion_body').is(':visible')) {
+			$(".accordion_body").slideUp(300);
+			$(".plusminus").text('+');
+		}
+        if( $(this).next(".accordion_body").is(':visible')){
+            $(this).next(".accordion_body").slideUp(300);
+            $(this).children(".plusminus").text('+');
+        }else {
+            $(this).next(".accordion_body").slideDown(300);
+            $(this).children(".plusminus").text('-');
+        }
+	});
   });
 });
